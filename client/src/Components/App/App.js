1707ee81352a4d4de6fc443b954d147bfcb8b813
Evgeniy <ekukonen@gmail.com>
2021-07-01 00:21:31 +0300

    modificated register page

--- client/src/Components/App/App.js
+++ client/src/Components/App/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Header from "../Header/Header";
+import {BrowserRouter, Route} from "react-router-dom";
+
+import RegisterPage from '../RegisterPage/RegisterPage'
 
 const App = () => {
     return (
         <div>
             <Header/>
+            <BrowserRouter>
+                <Route path="/register/" component={RegisterPage}/>
+            </BrowserRouter>
         </div>
     )
 }
--- client/src/Components/RegisterPage/RegisterPage.js
+++ client/src/Components/RegisterPage/RegisterPage.js
@@ -2,10 +2,64 @@ import React from 'react';
 
 import {observer} from "mobx-react-lite";
 
+import './RegisterPage.scss'
+import RuText from '../../TextData/Ru/login-register.json';
+import EnText from '../../TextData/En/login-register.json';
+import language from "../../Store/LanguageStore";
+
 const RegisterPage = observer(() => {
+
+    let text = {}
+    language.language === "en" ?
+        text = EnText :
+        text = RuText;
+
     return (
-        <div>
-            
+        <div className="AuthPage">
+            <h1>{text.register}</h1>
+            <div className="Auth-section">
+                <div className="Auth-block">
+                    <div className="Auth-text-block">
+                        {text.name}
+                    </div>
+                    <div className="Auth-input-block">
+                        <input type="text" className="Auth-input"/>
+                    </div>
+                </div>
+                <div className="Auth-block">
+                    <div className="Auth-text-block">
+                        {text.email}
+                    </div>
+                    <div className="Auth-input-block">
+                        <input type="text" className="Auth-input"/>
+                    </div>
+                </div>
+                <div className="Auth-block">
+                    <div className="Auth-text-block">
+                        {text.password}
+                    </div>
+                    <div className="Auth-input-block">
+                        <input type="password" className="Auth-input"/>
+                    </div>
+                </div>
+                <div className="Auth-block">
+                    <div className="Auth-text-block">
+                        {text.password}
+                    </div>
+                    <div className="Auth-input-block">
+                        <input type="password" className="Auth-input"/>
+                    </div>
+                </div>
+                <div className="Auth-block">
+                    <div className="Auth-text-block">
+                        {text.remember}
+                    </div>
+                    <div className="Auth-input-block">
+                        <input type="checkbox" className="Auth-checkbox"/>
+                    </div>
+                </div>
+            </div>
+            <input type="button" value={text.send}/>
         </div>
     )
 });
