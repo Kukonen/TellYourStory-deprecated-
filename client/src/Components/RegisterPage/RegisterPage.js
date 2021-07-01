8b7bea27c3dd08e3b6d4a4029ef2d981cfa93795
Evgeniy <ekukonen@gmail.com>
2021-07-01 17:04:36 +0300

    end register page design

--- client/src/Components/RegisterPage/RegisterPage.js
+++ client/src/Components/RegisterPage/RegisterPage.js
@@ -6,6 +6,7 @@ import './RegisterPage.scss'
 import RuText from '../../TextData/Ru/login-register.json';
 import EnText from '../../TextData/En/login-register.json';
 import language from "../../Store/LanguageStore";
+import user from '../../Store/UserState'
 
 const RegisterPage = observer(() => {
 
@@ -14,52 +15,178 @@ const RegisterPage = observer(() => {
         text = EnText :
         text = RuText;
 
+    const textBlockStyle = "Auth-text-block"
+    const textBlockStyleActive = "Auth-text-block Auth-text-block-active"
+    const inputStyle = "Auth-input"
+    const inputStyleActive = "Auth-input Auth-input-active"
+    const checkboxStyle = "Auth-checkbox"
+    const checkboxStyleActive = "Auth-checkbox Auth-checkbox-active"
+
+    let nameTextRef = React.createRef();
+    let nameInputRef = React.createRef();
+    let emailTextRef = React.createRef();
+    let emailInputRef = React.createRef();
+    let passwordOneTextRef = React.createRef();
+    let passwordOneInputRef = React.createRef();
+    let passwordTwoTextRef = React.createRef();
+    let passwordTwoInputRef = React.createRef();
+    let rememberTextRef = React.createRef();
+    let rememberInputRef = React.createRef();
+
     return (
         <div className="AuthPage">
             <h1>{text.register}</h1>
             <div className="Auth-section">
                 <div className="Auth-block">
-                    <div className="Auth-text-block">
+                    <div className={textBlockStyle}
+                         ref={nameTextRef}
+                    >
                         {text.name}
                     </div>
                     <div className="Auth-input-block">
-                        <input type="text" className="Auth-input"/>
+                        <input type="text" className={inputStyle}
+                               ref={nameInputRef}
+                               onFocus={
+                                   e => {
+                                       nameTextRef.current.className = textBlockStyleActive
+                                       e.target.className = inputStyleActive
+                                   }
+                               }
+                               onBlur={
+                                   e => {
+                                       nameTextRef.current.className = textBlockStyle
+                                       e.target.className = inputStyle
+                                   }
+                               }
+                               onChange={
+                                   value => {
+                                       user.registerChangeName(value.target.value)
+                                   }
+                               }
+                        />
                     </div>
                 </div>
                 <div className="Auth-block">
-                    <div className="Auth-text-block">
+                    <div className={textBlockStyle}
+                         ref={emailTextRef}
+                    >
                         {text.email}
                     </div>
                     <div className="Auth-input-block">
-                        <input type="text" className="Auth-input"/>
+                        <input type="text" className={inputStyle}
+                               ref={emailInputRef}
+                               onFocus={
+                                   e => {
+                                       emailTextRef.current.className = textBlockStyleActive
+                                       e.target.className = inputStyleActive
+                                   }
+
+                               }
+                               onBlur={
+                                   e => {
+                                       emailTextRef.current.className = textBlockStyle
+                                       e.target.className = inputStyle
+                                   }
+                               }
+                               onChange={
+                                   value => {
+                                       user.registerChangeEmail(value.target.value)
+                                   }
+                               }
+                        />
                     </div>
                 </div>
                 <div className="Auth-block">
-                    <div className="Auth-text-block">
+                    <div className={textBlockStyle}
+                         ref={passwordOneTextRef}
+                    >
                         {text.password}
                     </div>
                     <div className="Auth-input-block">
-                        <input type="password" className="Auth-input"/>
+                        <input type="password" className={inputStyle}
+                               ref={passwordOneInputRef}
+                               onFocus={
+                                   e => {
+                                       passwordOneTextRef.current.className = textBlockStyleActive
+                                       e.target.className = inputStyleActive
+                                   }
+
+                               }
+                               onBlur={
+                                   e => {
+                                       passwordOneTextRef.current.className = textBlockStyle
+                                       e.target.className = inputStyle
+                                   }
+                               }
+                               onChange={
+                                   value => {
+                                       user.registerChangePassword(value.target.value)
+                                   }
+                               }
+                        />
                     </div>
                 </div>
                 <div className="Auth-block">
-                    <div className="Auth-text-block">
+                    <div className={textBlockStyle}
+                         ref={passwordTwoTextRef}
+                    >
                         {text.password}
                     </div>
                     <div className="Auth-input-block">
-                        <input type="password" className="Auth-input"/>
+                        <input type="password" className={inputStyle}
+                               ref={passwordTwoInputRef}
+                               onFocus={
+                                   e => {
+                                       passwordTwoTextRef.current.className = textBlockStyleActive
+                                       e.target.className = inputStyleActive
+                                   }
+
+                               }
+                               onBlur={
+                                   e => {
+                                       passwordTwoTextRef.current.className = textBlockStyle
+                                       e.target.className = inputStyle
+                                   }
+                               }
+                               onChange={
+                                   value => {
+                                       user.registerChangePasswordAgain(value.target.value)
+                                   }
+                               }
+                        />
                     </div>
                 </div>
                 <div className="Auth-block">
-                    <div className="Auth-text-block">
+                    <div className={textBlockStyle}
+                        ref={rememberTextRef}
+                    >
                         {text.remember}
                     </div>
                     <div className="Auth-input-block">
-                        <input type="checkbox" className="Auth-checkbox"/>
+                        <input type="checkbox" className="Auth-checkbox"
+                               ref={rememberInputRef}
+                               onChange={e => {
+                                        let remember = e.target.checked
+                                        if (remember) {
+                                            e.target.className = checkboxStyleActive
+                                            rememberTextRef.current.className = textBlockStyleActive
+
+                                        }
+                                        else {
+                                            e.target.className = checkboxStyle
+                                            rememberTextRef.current.className = textBlockStyle
+                                        }
+                                        user.registerChangeRemember(remember)
+                                    }
+                               }
+                        />
                     </div>
                 </div>
             </div>
-            <input type="button" value={text.send}/>
+            <button type="button" className="Auth-button" onClick={() => user.register()}>
+                {text.send}
+            </button>
+
         </div>
     )
 });
--- client/src/Store/UserState.js
+++ client/src/Store/UserState.js
@@ -1,14 +1,32 @@
 import {makeAutoObservable} from "mobx";
+import axios from "axios";
 
 class UserState {
 
     name = ''
     isLogged = false
 
+    registerDATA = {
+        name: "",
+        email: "",
+        password: "",
+        passwordAgain: "",
+        remember: false
+    }
+
     constructor() {
         makeAutoObservable(this)
     }
 
+    registerChangeName = (value) => this.registerDATA.name = value
+    registerChangeEmail = (value) => this.registerDATA.email = value
+    registerChangePassword = (value) => this.registerDATA.password = value
+    registerChangePasswordAgain = (value) => this.registerDATA.passwordAgain = value
+    registerChangeRemember = (value) => this.registerDATA.remember = value
+
+    register() {
+        axios.post('/api/register', this.registerDATA)
+    }
 }
 
 export default new UserState();
