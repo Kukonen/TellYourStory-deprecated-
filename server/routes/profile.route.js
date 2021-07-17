67dc76ae67e76a823ea61fe99c36e30c23000a52
Evgeniy <ekukonen@gmail.com>
2021-07-17 18:05:33 +0300

    organizeted space

--- server/routes/profile.route.js
+++
@@ -1,8 +0,0 @@
-const Route = require('express')
-const route = new Route()
-const userController = require('../controllers/profile.controller')
-
-route.post('/changename', userController.changeName)
-route.post('/changepassword', userController.changePassword)
-
-module.exports = route;
