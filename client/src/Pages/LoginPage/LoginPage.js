a0e5abebdb78541c323e709c4c99c76aab3e3373
Evgeniy <ekukonen@gmail.com>
2021-07-19 22:05:38 +0300

    create cdefault font-family

--- client/src/Pages/LoginPage/LoginPage.js
+++ client/src/Pages/LoginPage/LoginPage.js
@@ -4,8 +4,17 @@ import {observer} from "mobx-react-lite";
 
 const LoginPage = observer(() => {
     return (
-        <div>
-
+        <div className="Auth-section">
+            <h1>Login</h1>
+            <div className="Auth-block">
+                email
+                <input className="Auth-input" type="text"/>
+            </div>
+            <div>
+                password
+                <input className="Auth-input" type="password"/>
+            </div>
+            <button className="Auth-button">login</button>
         </div>
     )
 });
