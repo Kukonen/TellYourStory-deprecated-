d190ecbdc793dc1a3da8d27269f4450951a7ffaa
Evgeniy <ekukonen@gmail.com>
2021-07-23 20:10:39 +0300

    add advanced errors

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -5,11 +5,15 @@ const uuid = require('uuid')
 class userController {
     async getInformation(ctx) {
         const key = ctx.cookies.get('key')
-        if (!key)
+        if (key === undefined) {
+            ctx.status = 204
             return;
+        }
         const user = await User.findOne({key: key})
-        if (!user)
+        if (!user){
+            ctx.status = 404
             return;
+        }
         ctx.status = 200
         ctx.body = {
             "name": user.name,
